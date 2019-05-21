4fe45cb0efe0f32d7b0e1a2012a7552aa23c8a1b
Jonathan <jjonathanlow@gmail.com>
2019-05-20 23:36:04 -0700

    updated code for 4x4 and LED matrix

--- arrowPad/arrowPad.ino
+++ arrowPad/arrowPad.ino
@@ -1,32 +1,101 @@
 #include <Keyboard.h>
+#include "Tlc5940.h"
 
-byte row[] = {7, 8, 10} ;//col grounds
-byte col[] = {3, 5, 6} ;//input pullups
-byte keysState[3][3];
+byte row[] = {7, 8, 0, 20} ;//col grounds
+byte col[] = {4, 1, 6, 21} ;//input pullups
+byte rowSize = sizeof(row);
+byte colSize = sizeof(col);
+byte keysState[4][4];
 
 //Edit this
 //change the key for each switch presse
 //refer to https://www.arduino.cc/en/Reference/KeyboardPress
 //modifier keys - https://www.arduino.cc/en/Reference/KeyboardModifiers
-char keys[3][3] =  {{'q', KEY_UP_ARROW, 'e'},
-                    {KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW},
-                    {'z', 'x', ' '}};
+char keys[4][4] =  {{'q', 'w', 'e', 'r'},
+                    {'a','s','d','f'},
+                    {'z', 'x', 'c', 'v'},
+                    {NULL, NULL, NULL, ' '}};
 
-byte rowSize = sizeof(row);
-byte colSize = sizeof(col);
+//ledMap[1] refers to the top left. ledMap[2] refers to the next one
+int ledMap[] = {7,6,4,5,11,10,9,8,15,14,13,12,0,1,2,3};
 
+int ledMode = 0;
+//0 = Fade
+//1 = Light pressed
+//2 = Light all except pressed
 void setup() {
   Serial.begin(9600);
   Keyboard.begin();
   initalize(); //sets up pins
+  Tlc.init(0);
 }
 
 void loop() {
   readBoard(); //reads and stores which switches are released and pressed 
   pressKeys(); //sends keypress to device
+  checkLightMode();
+  runLighting();
   //printState(); //for debugging
-  Serial.println(sizeof(keysState));
   delay(2); //to not over load the arduino
+}
+
+void runLighting(){
+  switch(ledMode){
+    case 0: lights();
+            break;
+    case 1: lightReact(0);
+            break;
+    case 2: lightReact(1);
+            break;
+  }
+}
+
+void checkLightMode(){
+  if(keysState[0][3] == 0){
+    ledMode = 0;
+  }
+  else if(keysState[1][3] == 0){
+    ledMode = 1;
+  }
+
+  else if(keysState[2][3] == 0){
+    ledMode = 2;
+  }
+}
+
+//vars for lights()
+int current = 0;
+int increment = 50;
+int inc = increment;
+
+void lights(){
+  current += inc;
+  if(current <= 0){
+    inc = increment;
+  }
+  else if(current >= 4000-increment){
+    inc = inc * -1;
+  }
+  setAll(current);
+  Serial.println(current);
+  Tlc.update();
+}
+
+//if pres = 1 Lights all but pressed
+void lightReact(int pres){
+  int lightIndex = 0;
+  for (int i = 0; i < rowSize; i++){
+    for(int j = 0; j < colSize; j++){
+      if(keysState[j][i] == pres){
+        Tlc.set(ledMap[lightIndex], 4000);
+      }
+      else{
+        Tlc.set(ledMap[lightIndex], 0);
+      }
+      lightIndex++;
+    }
+  }
+  Tlc.update();
 }
 
 //prints the keysState array
@@ -41,6 +110,15 @@ void printState(){
   }
 }
 
+void setAll(int x){
+  for (int i = 0; i < 16; i++) {
+    Tlc.set(i, x);
+  }
+}
+
+void setCol(int c, int val){
+  //for(int i = 0;)
+}
 //reads the keyboard matrix
 void readBoard(){
   for(int i = 0; i < rowSize; i++){ //iterate over each row
@@ -70,6 +148,7 @@ void pressKeys(){
         //sends pressed value based keys array
         Keyboard.press(keys[i][j]); 
       }
+      
       else{ //switch at current point is not pressed
         //Serial.print("Released: ");
         //Serial.println(keys[i][j]);
