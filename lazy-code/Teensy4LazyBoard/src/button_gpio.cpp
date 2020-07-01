a4186c4c95b7fa920e1f4a5fb18153473f37c87d
William Redenbaugh <wredenba@uci.edu>
2020-07-01 15:13:04 -0700

    added documentation for dip switches

--- lazy-code/Teensy4LazyBoard/src/button_gpio.cpp
+++ lazy-code/Teensy4LazyBoard/src/button_gpio.cpp
@@ -1,11 +1,20 @@
 #include "button_gpio.hpp"
-
+/**************************************************************************/
+/*!
+   @brief  Holds our latest dip switches. 
+*/
+/**************************************************************************/
 struct {
     uint8_t dip_one_val = 0; 
     uint8_t dip_two_val = 0; 
     uint8_t dip_three_val = 0; 
 }dip_val; 
 
+/**************************************************************************/
+/*!
+   @brief  Task that reads the dip switches
+*/
+/**************************************************************************/
 void dip_read_task(void *args){
     (void)args; 
     dip_val.dip_one_val = digitalReadFast(DIP1);
@@ -13,6 +22,11 @@ void dip_read_task(void *args){
     dip_val.dip_three_val = digitalReadFast(DIP3);
 }
 
+/**************************************************************************/
+/*!
+   @brief So we can setup the dip button subroutine
+*/
+/**************************************************************************/
 extern void setup_dip_button(void){    
     pinMode(DIP1, INPUT);
     pinMode(DIP2, INPUT);
