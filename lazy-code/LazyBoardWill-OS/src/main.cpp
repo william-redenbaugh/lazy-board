7fcfca8cfed497915a05003816dbe10552f5b446
William Redenbaugh <wredenba@uci.edu>
2020-08-20 18:46:58 -0700

    Started port of macropad to Will-OS

---
+++ lazy-code/LazyBoardWill-OS/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+// Including Protobuffer libraries 
+#include "pb.h"
+#include "pb_common.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
+
+// Including our OS stuff. 
+#include "OS/OSThreadKernel.h"
+#include "OS/OSSignalKernel.h"
+#include "OS/OSMutexKernel.h"
+
+// #include our Serial interfacing drivers 
+#include "HAL/OSSerial.h"
+
+#include "MODULES/LPWORK/lp_work_thread.h"
+#include "MODULES/PROTOCALLBACKS/teensy_coms.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
